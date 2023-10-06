cf8789382a7102ae8822b8c4d411bc1846ed26cc
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-06 17:39:51 +0530

    promiess done

--- Promises.js
+++ Promises.js
@@ -80,3 +80,24 @@ async function consumePromiseFive() {
     }
 }
 consumePromiseFive();
+
+// Fetch response with try catch
+async function getAllUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.log("error :", error);
+    }
+}
+
+getAllUsers();
+
+// fetch with .then .catch
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=> {
+    return response.json()
+})
+.then((data)=>{console.log(data)})
+.catch((err)=>{console.log(err)})
